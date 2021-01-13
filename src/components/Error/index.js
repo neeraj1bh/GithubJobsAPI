3b6e3ef18f1f84be4e0d22b4ce8a2c3602dba91c
neeraj1bh <neeraj1bh@gmail.com>
2021-01-14 01:25:34 +0530

    Added Spinner and Error

---
+++ src/components/Error/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Error.css";
+
+const Error = () => (
+  <div className="outerErrorDiv">
+    <div className="mainErrorDiv">Sorry No Results Found...</div>
+  </div>
+);
+
+export default Error;
---
+++ src/components/Spinner/index.js
@@ -0,0 +1,10 @@
+import "./Spinner.css";
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="mainSpinnerDiv">
+      <div className="spinnerDiv"></div>
+    </div>
+  );
+}

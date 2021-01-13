3c67e365726aef703ad7fd8bd6311915e6844279
neeraj1bh <neeraj1bh@gmail.com>
2021-01-13 12:51:47 +0530

    CHanged styles and fetching

--- src/components/App.js
+++ src/components/App.js
@@ -42,38 +42,46 @@ function App() {
   }, [theme]);
 
   return (
-    <div className="App">
-      <nav className="navHead">
-        <div className="logoTitle">
-          <h1>Dev Jobs</h1>
-        </div>
-        <div className="switch-wrapper">
-          <div className="sun"></div>
-          <div className="toggle-wrapper">
-            <input
-              id="switch"
-              type="checkbox"
-              checked={theme === "dark-theme" ? true : false}
-              onChange={toggleTheme}
-            />
-            <label htmlFor="switch" id="toggle">
-              Toggle
-            </label>
+    <>
+      <div className="navContainer">
+        <nav className="navHead">
+          <div className="logoTitle">
+            <h1>Dev Jobs</h1>
           </div>
-          <div className="moon"></div>
-        </div>
-      </nav>
-      <FormInput params={params} onParamChange={handleParamChange} />
-      <div className="containerAll">
-        <Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
-        {loading && <h2>Loading...</h2>}
-        {error && <h2>Error Try Refresh</h2>}
-        {jobs.map((job) => {
-          return <Job key={job.id} job={job} />;
-        })}
-        <Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+          <div className="switch-wrapper">
+            <div className="sun"></div>
+            <div className="toggle-wrapper">
+              <input
+                id="switch"
+                type="checkbox"
+                checked={theme === "dark-theme" ? true : false}
+                onChange={toggleTheme}
+              />
+              <label htmlFor="switch" id="toggle">
+                Toggle
+              </label>
+            </div>
+            <div className="moon"></div>
+          </div>
+        </nav>
       </div>
-    </div>
+      <div className="App">
+        <FormInput params={params} onParamChange={handleParamChange} />
+        {hasNextPage && !error && (
+          <Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+        )}
+        <div className="allJobs">
+          {loading && <h2>Loading...</h2>}
+          {error && <h2>Error Try Refresh</h2>}
+          {jobs.map((job) => {
+            return <Job key={job.id} job={job} theme={theme} />;
+          })}
+        </div>
+        {hasNextPage && !error && (
+          <Pagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+        )}
+      </div>
+    </>
   );
 }
 

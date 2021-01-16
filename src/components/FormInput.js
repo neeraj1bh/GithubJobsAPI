5433bf2f46d66aac1a3e9678dd86dbdb4485af66
neeraj1bh <neeraj1bh@gmail.com>
2021-01-16 16:14:50 +0530

    Added new styles to FormInput and Improved the logic to fetch PartTime and FullTime jobs

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,33 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import "../styles/FormInput.css";
 
-export default function FormInput({ params, onParamChange }) {
+export default function FormInput({ onParamChange }) {
+  const [fulltime, setFulltime] = useState(getStoredState());
+  function getStoredState() {
+    //Default off becauseserver returns Fulltime and Part time
+    let fulltime = "off";
+    if (localStorage.getItem("fulltime")) {
+      fulltime = localStorage.getItem("fulltime");
+    }
+    return fulltime;
+  }
+
+  const toggleFullTime = () => {
+    if (fulltime === "on") {
+      setFulltime("off");
+    } else {
+      setFulltime("on");
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("fulltime", fulltime);
+  }, [fulltime]);
+
+  const handleClick = (e) => {
+    onParamChange(e);
+    toggleFullTime();
+  };
   return (
-    <div className="formInput">
-      <input
-        className="textInput"
-        type="text"
-        name="description"
-        onChange={onParamChange}
-        placeholder="Description [ eg: Senior React Developer ]"
-      />
-      <input
-        className="textInput"
-        type="text"
-        name="location"
-        onChange={onParamChange}
-        placeholder="Location [ eg: New York ]"
-      />
-      <div className="checkInput">
-        <label htmlFor="full_time">Only Full-Time Roles </label>
+    <>
+      <div className="formInput">
         <input
-          type="checkbox"
-          name="full_time"
+          className="textInput"
+          type="text"
+          name="description"
           onChange={onParamChange}
-          id="full-time"
+          placeholder="Description [ eg: Senior React Developer ]"
+        />
+
+        <button
+          className="buttonInput"
+          onClick={handleClick}
           label="Only Full Time"
+        >
+          {localStorage.getItem("fulltime") === "off"
+            ? "Full Time & Part Time"
+            : "Full Time"}
+        </button>
+        <input
+          className="textInput"
+          type="text"
+          name="location"
+          onChange={onParamChange}
+          placeholder="Location [ eg: New York ]"
         />
       </div>
-    </div>
+    </>
   );
 }
